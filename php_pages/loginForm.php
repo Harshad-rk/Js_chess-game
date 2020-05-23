fe6c2e572e6789728d001cd418e5413bacd1a06f
Harshad-rk <harshad.r.k.100@gmail.com>
2020-05-23 19:08:13 +0530

    all file

---
+++ php_pages/loginForm.php
@@ -0,0 +1,79 @@
+<!-- Login------------------------------------------------------------>	
+	<div id="infoSQL" ></div>
+	<input type="submit" id="logout_submit" value="logout" style="display:none">
+<!-- Form------------------------------------------------------------>	
+<input type="submit" id="registerButton" value="register">
+
+<!-- Form------------------------------------------------------------>	
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+ Login
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       
+		<div type="text" id="logform">
+			<label for="username">Username</label>
+			<input type="text" id="username" />
+			<br/>
+			<label for="password">Password</label>
+			<input type="password" id="password" />
+			<br/>
+			<br/>
+                </div>	
+		
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" id="submit" value="login">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Button trigger modal -->
+<button  type="submit" id="registerButton" value="register" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter1">
+  Register
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenter1Title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Register</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <div type="text" id="form_container" method="post" style="display:none">
+	<div class="field">
+		<label for="regusername">Choose a username</label>
+		<input type="text" name="regusername" id="regusername" value="">
+	</div>
+	<div class="field">
+		<label for="regpassword">Choose a password</label>
+		<input type="password" name="regpassword" id="regpassword">
+	</div>
+	<div class="field">
+		<label for="regpassword_again">Enter your password again</label>
+		<input type="password" name="regpassword_again" id="regpassword_again">
+	</div>
+	
+	<input type="submit" id="register_submit" value="Register">
+</div>	
+      </div>
+      
+    </div>
+  </div>
+</div>
---
+++ php_pages/notOutputted.php
@@ -0,0 +1,22 @@
+<div id="EngineOutput"><br/>
+			<select id="ThinkTimeChoice" style="">
+			  <option value="1">1s</option>
+			  <option value="2">2s</option>
+			  <option value="4">4s</option>
+			  <option value="6">6s</option>
+			  <option value="8">8s</option>
+			  <option value="10">10s</option>
+			</select><br/><br/><br/>
+	
+			<span id="BestOut" style="">BestMove:</span><br/>
+			<span id="DepthOut"style="">Depth:</span><br/>
+			<span id="ScoreOut"style="">Score:</span><br/>
+			<span id="NodesOut"style="">Nodes:</span><br/>
+			<span id="OrderingOut"style="">Ordering:</span><br/>
+			<span id="TimeOut"style="">Time:</span><br/><br/>
+			<button type="button" id="SearchButton"style="">Move Now</button><br/>
+			
+			<button type="button" id="FlipButton"style="">Flip Board</button><br/><br/>
+			<button type="button" id="TakeButton"style="">Take Back</button><br/><br/><br/>
+			
+</div>
